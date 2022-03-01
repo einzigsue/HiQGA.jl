0e5fa24b26d7fb3daf09185da4641d2c4e1d57ec
richardt94 <29562583+richardt94@users.noreply.github.com>
2022-03-02 10:18:55 +1100

    add inversion nuisance for constant phase offset

--- src/SMRPI.jl
+++ src/SMRPI.jl
@@ -4,6 +4,7 @@ module SMRPI
 using ..AbstractOperator, ..CommonToAll
 import ..AbstractOperator.get_misfit
 import ..Model, ..Options
+import ..ModelNuisance, ..OptionsNuisance
 using SNMRForward, Random
 
 abstract type SMRSounding <: Operator1D end
@@ -22,9 +23,10 @@ mutable struct SMRSoundingUnknown <: SMRSounding
     V0 :: Vector{<:Real}
     ϕ :: Vector{<:Real}
     Fm :: SNMRForward.MRSForward
+    σd_diag :: Vector{<:Real}
 end
 
-function newSMRSounding(V0, ϕ, Fm; σ_V0=nothing, σ_ϕ=nothing)
+function newSMRSounding(V0, ϕ, Fm; σ_V0=nothing, σ_ϕ=nothing, mult=false)
     (length(V0) != length(ϕ) && 
     throw(ArgumentError("V0 and ϕ must have same length")))
     if !isnothing(σ_V0) || !isnothing(σ_ϕ)
@@ -37,33 +39,48 @@ function newSMRSounding(V0, ϕ, Fm; σ_V0=nothing, σ_ϕ=nothing)
         return SMRSoundingKnown(V0, ϕ, σ_V0, σ_ϕ, Fm)
     end
 
-    SMRSoundingUnknown(V0, ϕ, Fm)
+    if mult
+        σd_diag = [S.V0; ones(length(ϕ))]
+    else
+        σd_diag = [ones(length(V0)); 1 ./ V0]
+    end
+    SMRSoundingUnknown(V0, ϕ, Fm, σd_diag)
 end
 
 function get_misfit(m::Model, opt::Options, S::SMRSounding)
     opt.debug && return 0.0
-    get_misfit(10 .^m.fstar[:], S)
+    get_misfit(10 .^ m.fstar[:], S)
 end
 # above defined function and type signature MUST be defined
 
-function get_misfit(w::Vector{<:Real}, S::SMRSounding)
+function get_misfit(m::Model, mn::ModelNuisance, opt::Union{Options,OptionsNuisance}, S::SMRSounding)
+    #the "nuisance" in this case is a constant offset phase
+    opt.debug && return 0.0
+    offset_ϕ = mn.nuisance[1]
+    get_misfit(10 .^ m.fstar[:], S, offset_ϕ = offset_ϕ)
+end
+
+function get_misfit(w::Vector{<:Real}, S::SMRSounding; offset_ϕ = 0.)
     response = SNMRForward.forward(S.Fm, w)
     Vres = abs.(response)
-    ϕres = angle.(response)
+    ϕres = angle.(response) .+ offset_ϕ
     if isa(S, SMRSoundingKnown)
         #use provided noise
-        residual = [(S.V0 .- Vres)./S.σ_V0; (S.ϕ .- ϕres)./S.σ_ϕ]
+        residual = [(S.V0 .- Vres)./S.σ_V0;
+                rem2pi.(S.ϕ - ϕres, RoundNearest)./S.σ_ϕ]
         return residual' * residual / 2
     else
         #maximum-likelihood estimate of multiplicative noise
-        residual = [(S.V0 - Vres)./ S.V0; S.ϕ - ϕres]
-        return length(residual)/2 * residual' * residual
+        residual = [S.V0 - Vres;
+                rem2pi.(S.ϕ - ϕres, RoundNearest)]./S.σd_diag
+        return length(residual)/2 * log(residual' * residual)
     end
 end
 
 function create_synthetic(w::Vector{<:Real}, σ::Vector{<:Real}, t::Vector{<:Real},
             Be::Real, ϕ::Real, R::Real, zgrid::Vector{<:Real}, qgrid::Vector{<:Real}
-    ; noise_frac = 0.05, θ = 0, square=false, noise_mle = false)
+    ; noise_frac = 0.05, θ = 0., square=false, noise_mle = false, mult = false,
+    offset_ϕ = 0.)
     ct = SNMRForward.ConductivityModel(σ, t)
 
     F = (square ?
@@ -71,17 +88,23 @@ function create_synthetic(w::Vector{<:Real}, σ::Vector{<:Real}, t::Vector{<:Rea
         SNMRForward.MRSForward_square(R, zgrid, qgrid, ϕ, θ, Be, ct))
 
     synth_data = SNMRForward.forward(F,w)
-    σ_V0 = noise_frac * abs.(synth_data)
-    σ_ϕ = noise_frac * ones(size(synth_data))
+    if mult
+        σ_V0 = noise_frac * abs.(synth_data)
+        σ_ϕ = noise_frac * ones(size(synth_data))
+    else
+        σ = noise_frac * maximum(abs.(synth_data))
+        σ_V0 = σ * ones(size(synth_data))
+        σ_ϕ = σ ./ abs.(synth_data)
+    end
     noisy_V0 = abs.(synth_data) .+ σ_V0 .* randn(size(abs.(synth_data)))
-    noisy_ϕ = angle.(synth_data) .+ σ_ϕ .* randn(size(abs.(synth_data)))
+    noisy_ϕ = angle.(synth_data) .+ σ_ϕ .* randn(size(abs.(synth_data))) .+ offset_ϕ
 
     if noise_mle
         σ_V0 = nothing
         σ_ϕ = nothing
     end
 
-    newSMRSounding(noisy_V0, noisy_ϕ, F, σ_V0=σ_V0, σ_ϕ=σ_ϕ)
+    newSMRSounding(noisy_V0, noisy_ϕ, F, σ_V0=σ_V0, σ_ϕ=σ_ϕ, mult=mult)
 end
 
 
